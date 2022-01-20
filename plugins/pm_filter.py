3de1478221081e3f9640de0bcabfbdb07da68d1e
Don <96370825+SflixBots@users.noreply.github.com>
2022-01-20 11:16:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -703,7 +703,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.CORONA_TXT,
+            text=Script.CORONA_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
